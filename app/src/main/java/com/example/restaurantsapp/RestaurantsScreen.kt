6623412283bffcdc382e85e82eadb1781c651620
tunmie <victoroluwasomidotun@gmail.com>
2022-07-04 01:44:04 +0100

    icon state done

--- app/src/main/java/com/example/restaurantsapp/RestaurantsScreen.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.restaurantsapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,21 +12,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun RestaurantsScreen(){
+	val viewModel: RestaurantsViewModel = viewModel()
 	LazyColumn(contentPadding = PaddingValues(
 		vertical = 8.dp,
 		horizontal = 8.dp
 	)) {
-		items(dummyRestaurants) {restaurant ->
+		items(viewModel.getRestaurants()) {restaurant ->
 			RestaurantItem(item = restaurant)
 		}
 	}
@@ -47,10 +54,31 @@ fun RestaurantItem(item: Restaurant) {
 			RestaurantDetails(
 				item.title,
 				item.description,
-				Modifier.weight(0.85f)
+				Modifier.weight(0.7f)
 			)
+			FavoriteIcon(Modifier.weight(0.15f))
 		}
 	}
+}
+
+@Composable
+fun FavoriteIcon(modifier: Modifier) {
+	val favoriteState = remember {
+		mutableStateOf(false)
+	}
+	val icon = if (favoriteState.value)
+		Icons.Filled.Favorite
+	else
+		Icons.Filled.FavoriteBorder
+	Image(
+		imageVector = icon,
+		contentDescription = "Favorite restaurant icon",
+		modifier = modifier
+			.padding(8.dp)
+			.clickable {
+				favoriteState.value = !favoriteState.value
+			}
+	)
 }
 
 @Composable
